4370c2d09cc2edfba00391022a99102ea476bf5a
RonanAra <ronanlm10@hotmail.com>
2026-07-23 16:00:08 -0300

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Ronan Fernandes OLX on 23/07/26.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
